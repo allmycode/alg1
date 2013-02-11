83dc69293dd66df6046ddf2ecea65721dc71fff8
Stanisalv Lyakhnovich <stanisalv.lyakhnovich@db.com>
2013-02-11 17:15:35 +0400

    Main

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        for (int k = 0; k < 10; k++) {
-            Percolation p = new Percolation(1000);
-            for (int i = 1; i <= 1000; i++) {
-                for (int j = 1; j <= 1000; j++) {
-                    if (StdRandom.random() < 0.593) {
-                        p.open(i, j);
-                        if (p.percolates()) {
-                            break;
-                        }
-                    }
-                }
-                if (p.percolates()) {
-                    break;
-                }
+        int N = 2;
+        Percolation p = new Percolation(N);
+        int c = 0;
+        while (!p.percolates()) {
+            int i = StdRandom.uniform(1, N+1);
+            int j = StdRandom.uniform(1, N+1);
+            if (!p.isOpen(i, j)) {
+                p.open(i, j);
+                c++;
             }
-            end:
-            if (p.percolates())
-                System.out.println("percolates");
-            else
-                System.out.println("not percolates");
         }
+        System.out.println(c);
     }
 }
