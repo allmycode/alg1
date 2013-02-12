982f5e712ce3389c2f6d39ac9cb621e3921e4c71
Stanisalv Lyakhnovich <stanisalv.lyakhnovich@db.com>
2013-02-12 18:39:04 +0400

    different test

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        int N = 2;
-        Percolation p = new Percolation(N);
-        int c = 0;
-        while (!p.percolates()) {
-            int i = StdRandom.uniform(1, N+1);
-            int j = StdRandom.uniform(1, N+1);
-            if (!p.isOpen(i, j)) {
-                p.open(i, j);
-                c++;
-            }
-        }
-        System.out.println(c);
+        Percolation p = new Percolation(3);
+        p.open(2,3);
+        p.open(2,2);
+        p.open(3,2);
+        p.open(3,1);
+        assert p.isFull(3,1) == false;
+
     }
 }
